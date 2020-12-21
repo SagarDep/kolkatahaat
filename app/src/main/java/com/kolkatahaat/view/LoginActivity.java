bd0658637abec5d4a9123f08b1079986e2170b5e
Sagar Kaneriya <sagar.apsolute@gmail.com>
2020-12-21 22:46:37 +0530

    add login and registration screen design

---
+++ app/src/main/java/com/kolkatahaat/view/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.kolkatahaat.view;
+
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.kolkatahaat.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    public void setContentView(View view) {
+        super.setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/kolkatahaat/view/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.kolkatahaat.view;
+
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.kolkatahaat.R;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    public void setContentView(View view) {
+        super.setContentView(R.layout.activity_register);
+    }
+}
