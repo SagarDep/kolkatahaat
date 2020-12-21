1046f210c5ac46ea2b109154c53a5cc64a84eb9d
Sagar Kaneriya <sagar.apsolute@gmail.com>
2020-12-21 22:58:01 +0530

    add home admin activity and customer activity with both layout

---
+++ app/src/main/java/com/kolkatahaat/view/HomeAdminActivity.java
@@ -0,0 +1,15 @@
+package com.kolkatahaat.view;
+
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.kolkatahaat.R;
+
+public class HomeAdminActivity extends AppCompatActivity {
+
+    @Override
+    public void setContentView(View view) {
+        super.setContentView(R.layout.activity_home_admin);
+    }
+}
